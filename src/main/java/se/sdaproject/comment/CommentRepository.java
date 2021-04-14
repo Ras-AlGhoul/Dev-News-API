f63f5c6e8a7482ab9087510fb80a6483abe48ae7
Yazan <yazann.abbas@gmail.com>
2021-04-14 11:44:35 +0200

    @Repostirory

--- src/main/java/se/sdaproject/comment/CommentRepository.java
+++ src/main/java/se/sdaproject/comment/CommentRepository.java
@@ -2,9 +2,11 @@ package se.sdaproject.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
     List<Comment> findByAuthorName(@Param("name") String name);
 }
